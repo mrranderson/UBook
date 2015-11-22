184a15d4a2a002e8c68b84f509acb3244b1d1b46
Ryan Anderson <rsa5bb@virginia.edu>
2015-11-22 18:27:12 -0500

    Updated form, added validation and errors

--- UBook/models.py
+++ UBook/models.py
@@ -1,13 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
+from django.core import validators
+import re
 
 class UBookProfile(models.Model):
     user = models.OneToOneField(User)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    zipcode = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    state = models.CharField(max_length=255, blank=True)
+    zipcode = models.CharField(max_length=255, blank=True)
+    cc_type = models.CharField(max_length=20, choices=((1, "Visa"), (2, "Mastercard"), (3, "American Express"), (4, "Discover")), default=1)
+    cc_number = models.CharField(max_length=20, blank=True)
+    cc_expdate = models.CharField(max_length=10, blank=True)
+    cc_ccv = models.CharField(max_length=3, blank=True)
 
     def __str__(self):
         return self.user.username
 
+class UBookProfileForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    password = forms.CharField(max_length=255)
+    email = forms.CharField(max_length=255, validators=[validators.validate_email])
+    first_name = forms.CharField(max_length=255)
+    last_name = forms.CharField(max_length=255)
+    address = forms.CharField(max_length=255)
+    city = forms.CharField(max_length=255)
+    state = forms.CharField(max_length=255)
+    zipcode = forms.CharField(max_length=255)
+    cc_type = forms.CharField(max_length=16)
+    cc_number = forms.CharField(max_length=20)
+    cc_expdate = forms.CharField(max_length=10)
+    cc_ccv = forms.CharField(max_length=3)
+
+    def clean_zipcode(self):
+        data = self.cleaned_data['zipcode']
+        if not re.match('\d\d\d\d\d(-\d\d\d\d)?', data):
+            raise forms.ValidationError("Please enter a valid zipcode in the form of 12345 or 12345-6789.")
+        return data
+    
+    def clean_cc_number(self):
+        data = self.cleaned_data['cc_number']
+        if not re.match('\d{16}', data):
+            raise forms.ValidationError("Please enter a valid credit card number in the form of 0000111122223333.")
+        return data
+
+    def clean_cc_expdate(self):
+        data = self.cleaned_data['cc_expdate']
+        if not re.match('\d\d/[/-]d\d[/-]\d\d\d\d', data):
+            raise forms.ValidationError("Please enter a valid expiration date in the form of MM/DD/YYYY or MM-DD-YYYY.")
+        return data
+
+    def clean_cc_ccv(self):
+        data = self.cleaned_data['cc_ccv']
+        if not re.match('\d\d\d', data):
+            raise forms.ValidationError("Please enter a valid CCV in the form of 123.")
+        return data
--- UBook/views.py
+++ UBook/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
-from .models import (User, UBookProfile)
+from .models import User
+from UBook.models import UBookProfile, UBookProfileForm
 
 def index(request):
     return render(request, 'website/index.html', {})
@@ -13,6 +14,7 @@ def about(request):
 def signup(request):
 
     if request.method == 'POST':
+        """
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
@@ -22,17 +24,47 @@ def signup(request):
         city = request.POST['city']
         state = request.POST['state']
         zipcode = request.POST['zipcode']
+        cc_type = request.POST['cc_type']
+        cc_number = request.POST['cc_number']
+        cc_expdate = request.POST['cc_expdate']
+        cc_ccv = request.POST['cc_ccv']
+        """
 
-        new_user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        signup_form = UBookProfileForm(request.POST)
+       
+        if signup_form.is_valid():
+            new_user = User.objects.create_user(username=signup_form.cleaned_data['username'], 
+                password=signup_form.cleaned_data['password'], 
+                email=signup_form.cleaned_data['email'], 
+                first_name=signup_form.cleaned_data['first_name'], 
+                last_name=signup_form.cleaned_data['last_name'])
 
-        new_ubook = UBookProfile(user=new_user, address=address, city=city, state=state, zipcode=zipcode)
+            new_ubook = UBookProfile(user=new_user, 
+                address=signup_form.cleaned_data['address'], 
+                city=signup_form.cleaned_data['city'], 
+                state=signup_form.cleaned_data['state'], 
+                zipcode=signup_form.cleaned_data['zipcode'], 
+                cc_type=signup_form.cleaned_data['cc_type'], 
+                cc_number=signup_form.cleaned_data['cc_number'], 
+                cc_expdate=signup_form.cleaned_data['cc_expdate'], 
+                cc_ccv=signup_form.cleaned_data['cc_ccv'])
+            new_user.save()
+            new_ubook.save()
+            return HttpResponseRedirect(reverse('index'))
 
-        new_ubook.save()
+        else:
+            print("FUCKIN SHIT UP")
+            print(signup_form.errors)
+            return render(request, 'website/signUp.html', {
+                'error':True,
+                'errormsg':"Input Not Valid",
+                'form':signup_form
+            })
 
-        return HttpResponseRedirect(reverse('index'))
 
 
 
     if request.method == 'GET':
-        return render(request, 'website/signUp.html', {})
+        form = UBookProfileForm()
+        return render(request, 'website/signUp.html', {"form":form})
 
